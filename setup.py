7890dbfd5e624bc525a2559731f5ece547952d87
Tomekske <joostenstomek@gmail.com>
2018-05-17 20:36:01 +0200

    log: 15
    Added:
    - __init__.py
    - setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='ConfigHandler',
+	author='Tomek Joostens',
+	author_email='joostenstomek@gmail.com',
+	url='https://github.com/Tomekske/Module_ConfigHandler',
+	version='1.0.4',
+	py_modules=['ConfigHandler'],
+	install_requires=['configparser']
+)
